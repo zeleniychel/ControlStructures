d5af8e08e0995a3331966b38b13b4d5c5f5bb993
Anton Kamnev AN44 <gish18@gmail.com>
2023-05-20 00:06:57 +0300

    add algorithm

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,28 @@
 fun main() {
-    println(agoToText(150000))
+    println(agoToText(86360))
 }
 
 fun agoToText(time: Int) = when (time) {
     in 0..60 -> "был(а) только что"
     in 61..3599 -> "был(а) в сети ${time / 60} ${minuteAgo(time)} назад"
     in 3600..86359 -> "был(а) в сети ${time / 3600} ${hoursAgo(time)} назад"
-    in 86400..172799 -> "был(а) в сети вчера"
+    in 86360..172799 -> "был(а) в сети вчера"
     in 172800..259199 -> "был(а) в сети позавчера"
     else -> "был(а) в сети давно"
 }
 
-fun minuteAgo (time: Int) = when (time / 60) {
-    1,21,31,41,51 -> "минуту"
-    2,3,4,22,23,24,32,33,34,42,43,44,52,53,54 -> "минуты"
+fun minuteAgo (time: Int) = when {
+    time / 60 == 1 -> "минуту"
+    time / 60 in 2..4 -> "минуты"
+    time / 60 in 4..20 -> "минут"
+    (time / 60) % 10 == 1 -> "минуту"
     else -> "минут"
 }
 
-fun hoursAgo (time: Int) = when (time / 3600) {
-    1, 21 -> "час"
-    2, 3, 4, 22, 23 -> "часа"
+fun hoursAgo (time: Int) = when {
+    time / 3600 == 1 -> "час"
+    time / 3600 in 2..4 -> "часа"
+    time / 3600 in 5..20 -> "часов"
+    (time / 3600) % 10 == 1 -> "час"
     else -> "часов"
 }
